4c83888ff8252951f4f80e086e04ff61755692a6
v-ziruifang <v-ziruifang@microsoft.com>
2021-04-09 18:08:10 +0800

    return post

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import json
 
 from flask import Flask, request, jsonify
-
+import object_detection
 app = Flask(__name__)
 
 
 @app.route('/api/detectImage', methods=["POST"])
 def detectImage():
     record = json.loads(request.data)
-    objectId = record['id']
+    objectId = record['url']
+    res = object_detection.detectImage(objectId)
     """
 
     :rtype: json
     """
-    return json.dumps({'id': '1111', }), 200
+    return json.dumps(res), 200
 
 
 if __name__ == '__main__':
